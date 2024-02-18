4e8a4fb2fc7e5d66d04c56e405a68f298f452838
Matthew Ticciati <mticciati@gmail.com>
2024-02-17 16:40:38 -0800

    config and connect pg db with tags entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from '@app/ormconfig';
 
 @Module({
-  imports: [TagModule],
+  imports: [
+    TypeOrmModule.forRoot(config), 
+    TagModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
